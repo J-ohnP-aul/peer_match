2241714046b565578ec01b865f7f62c3611dff1b
J-ohnP-aul <johnpaulnoblemen@gmail.com>
2026-09-14 16:41:08 +0300

    build the academis profile completion based on regNo

---
+++ academics/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+
+class School(models.Model):
+    name = models.CharField(max_length=150)
+    code = models.CharField(max_length=20, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.code} - {self.name}"
+      
+class Course(models.Model):
+  school = models.ForeignKey(School, on_delete=models.CASCADE, related_name="courses")
+  name = models.CharField(max_length=150)
+  code = models.CharField(max_length=20, unique=True)
+  reg_prefix = models.CharField(max_length=10, unique=True)  #in16 sftware
+  description = models.TextField(blank=True)
+  
+  def __str__(self):
+     return f"{self.code} - {self.name}"
+  
+class Unit(models.Model):
+  course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="units")
+  code = models.CharField(max_length=20)
+  name = models.CharField(max_length=150)
+
+  year = models.PositiveSmallIntegerField()
+  semester = models.PositiveSmallIntegerField()
+
+  def __str__(self):
+      return f"{self.code} - {self.name}"
+
+class StudentUnit(models.Model):  # record the units that belong to a particular student
+  student = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="academic_units")
+  unit = models.ForeignKey(Unit, on_delete=models.CASCADE, related_name="students")
+  class Meta:
+    constraints = [
+        models.UniqueConstraint(
+            fields=["student", "unit"],
+            name="unique_student_unit"
+        )
+    ]
+
+  def __str__(self):
+      return f"{self.student} - {self.unit}"
+    
--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', include('core'))
     path('account/', include('account.urls')),
+    
 ]
